812ddaf06079093204cf6642d64a5eecc311e26a
Robert Jackson <rjackson@linkedin.com>
2019-10-16 19:34:56 -0400

    eslint . --fix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import * as core from '@actions/core';
-import {wait} from './wait'
+import { wait } from './wait';
 
 async function run() {
   try {
     const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    console.log(`Waiting ${ms} milliseconds ...`);
 
-    core.debug((new Date()).toTimeString())
+    core.debug(new Date().toTimeString());
     await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
+    core.debug(new Date().toTimeString());
 
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
--- src/wait.ts
+++ src/wait.ts
@@ -1,10 +1,9 @@
 export function wait(milliseconds: number) {
-    return new Promise((resolve) => {
-        if (isNaN(milliseconds)) { 
-            throw new Error('milleseconds not a number'); 
-        }
+  return new Promise(resolve => {
+    if (isNaN(milliseconds)) {
+      throw new Error('milleseconds not a number');
+    }
 
-        setTimeout(() => resolve("done!"), milliseconds)
-    });
+    setTimeout(() => resolve('done!'), milliseconds);
+  });
 }
-  
